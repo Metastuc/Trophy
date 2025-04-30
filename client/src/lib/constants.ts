d91494232345ad9e3eae4e38ef1959813733094a
cakezero <sareahj.com@gmail.com>
2025-04-30 02:07:22 +0100

    updated constants to fetch network based on NEXT_ENV and pinata jwt token .env fetch

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,5 +1,11 @@
+import { base, baseSepolia } from "viem/chains";
+
 export const ENV_SCHEMA = {
     PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID as string,
+    ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT || "development",
+    PINATA_JWT: process.env.NEXT_PUBLIC_PINATA_JWT as string,
 };
 
 export const PUBLIC_ROUTES = ["/auth"];
+
+export const network = ENV_SCHEMA.ENVIRONMENT === "development" ? baseSepolia : base;
