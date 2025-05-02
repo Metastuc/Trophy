50bcfcdd7dbb2c1dbd3a8a835464113e3753a76b
cakezero <sareahj.com@gmail.com>
2025-05-02 21:53:29 +0100

    paymaster setup

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -4,6 +4,8 @@ export const ENV_SCHEMA = {
     PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID as string,
     ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT || "development",
     PINATA_JWT: process.env.NEXT_PUBLIC_PINATA_JWT as string,
+    PAYMASTER_API_KEY: process.env.NEXT_PUBLIC_PAYMASTER_API_KEY as string,
+    PAYMASTER_URL: process.env.NEXT_PUBLIC_PAYMASTER_URL!
 };
 
 export const PUBLIC_ROUTES = ["/auth"];
