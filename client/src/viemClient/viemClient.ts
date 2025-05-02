3ac6eb7365f928bd1d18bc4ada63dbe3da944a54
cakezero <sareahj.com@gmail.com>
2025-05-02 21:55:33 +0100

    viem wallet client update to recieve provider

--- client/src/viemClient/viemClient.ts
+++ client/src/viemClient/viemClient.ts
@@ -1,6 +1,13 @@
-import { createPublicClient, createWalletClient, http, custom, type PublicClient, type WalletClient } from "viem";
-import { ConnectedWallet } from "@privy-io/react-auth";
 import { network } from "@/lib/constants";
+import { EIP1193Provider } from "@privy-io/react-auth";
+import {
+  type PublicClient,
+  type WalletClient,
+  createPublicClient,
+  createWalletClient,
+  custom,
+  http,
+} from "viem";
 
 let walletClient: WalletClient | undefined;
 
@@ -9,11 +16,10 @@ export const getPublicClient = () => {
     chain: network,
     transport: http(),
   }) as PublicClient;
-}
+};
 
-export const getWalletClient = async (wallet: ConnectedWallet) => {
+export const getWalletClient = async (provider: EIP1193Provider) => {
   if (!walletClient) {
-    const provider = await wallet.getEthereumProvider();
     walletClient = createWalletClient({
       chain: network,
       transport: custom(provider!),
@@ -21,4 +27,4 @@ export const getWalletClient = async (wallet: ConnectedWallet) => {
   }
 
   return walletClient;
-}
+};
