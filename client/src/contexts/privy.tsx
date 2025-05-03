02683a622147139576631dd4f389a4cf92846909
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-05-03 13:55:21 +0100

    feat: integrate Privy authentication; add environment schema and context provider; update authentication drawer styles and types

---
+++ client/src/contexts/privy.tsx
@@ -0,0 +1,8 @@
+import { ENV_SCHEMA } from "@/lib/constants";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+import React from "react";
+
+export function PrivyContextProvider({ children }: { children: React.ReactNode }) {
+    return <PrivyProvider appId={ENV_SCHEMA.PRIVY_APP_ID}>{children}</PrivyProvider>;
+}
