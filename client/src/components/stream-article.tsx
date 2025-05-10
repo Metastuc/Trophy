d7f9411a9285e00778be29bb38147eb417b1ed54
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-05-10 09:08:19 +0100

    feat: add StreamArticle component with context and skeleton UI; update index route to display articles

--- client/src/components/stream-article.tsx
+++ client/src/components/stream-article.tsx
@@ -1,5 +1,76 @@
+import React from "react";
+
 interface iStreamArticle {}
 
+const StreamArticleContext = React.createContext<iStreamArticle | null>(null);
+
+function useStreamArticleContext() {
+    const context = React.useContext(StreamArticleContext);
+
+    if (!context)
+        throw new Error(
+            "useStreamArticleContext must be used within a StreamArticleContextProvider",
+        );
+    return context;
+}
+
+function Header() {
+    const {} = useStreamArticleContext();
+
+    return (
+        <header className="flex h-8 items-center justify-between border border-purple-700">
+            <aside className="flex items-center gap-0.5">
+                <i className="size-8">
+                    <img
+                        src="https://www.dummyimage.com/200x200/000/fff"
+                        alt="user-pfp"
+                        className="rounded-full"
+                    />
+                </i>
+
+                <span className="text-xs">@username</span>
+            </aside>
+
+            <aside>
+                <button>
+                    <span className="capitalize">trade</span>
+                </button>
+            </aside>
+        </header>
+    );
+}
+
+function Main() {
+    const {} = useStreamArticleContext();
+
+    return <main></main>;
+}
+
+function Footer() {
+    const {} = useStreamArticleContext();
+
+    return (
+        <footer>
+            <aside>
+                <span>title</span>
+            </aside>
+
+            <aside>
+                <i>icon</i>
+                <span>watching</span>
+            </aside>
+        </footer>
+    );
+}
+
 export default function Component({}: iStreamArticle) {
-    return <article className={`h-72 border border-green-700`}>Component</article>;
+    return (
+        <StreamArticleContext.Provider value={null}>
+            <article className={`h-72 border border-green-700`}>
+                <Header />
+                <Main />
+                <Footer />
+            </article>
+        </StreamArticleContext.Provider>
+    );
 }
