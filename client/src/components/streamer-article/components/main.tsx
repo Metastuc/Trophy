9980b7b959ca212e6670a46eaab923989ebaf103
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-05-10 10:36:46 +0100

    feat: update bottom navigation links with correct hrefs and enhance skeleton UI in streamer article component

--- client/src/components/streamer-article/components/main.tsx
+++ client/src/components/streamer-article/components/main.tsx
@@ -7,12 +7,12 @@ export default function Component() {
 
     return (
         <main className="relative h-53 w-full">
-            <aside className="absolute right-2 top-2 z-10 flex h-7 w-15 items-center justify-center gap-2 rounded-full bg-white/90">
+            <aside className="absolute top-2 right-2 z-10 flex h-7 w-15 items-center justify-center gap-2 rounded-full bg-white/90">
                 <i>{LIVE()}</i>
                 <span className="text-[.625rem] font-semibold uppercase">live</span>
             </aside>
 
-            <Skeleton className="size-full" />
+            <Skeleton className="size-full rounded-none" />
         </main>
     );
 }
