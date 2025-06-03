65dc9ee698d4a0b38373ca1a29bd2e43a47f94f5
cakezero <sareahj.com@gmail.com>
2025-06-03 18:52:55 +0100

    chore: code refactor to use one entry point and connect to mongodb

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { PORT } from "./src/utils/env.js";
-import roomRoutes from "./src/routes/room.routes.js";
-import accessTokenRoutes from "./src/routes/accessToken.routes.js";
-import authUserRoutes from "./src/routes/authUser.routes.js";
-import signUpRoutes from "./src/routes/signUp.routes.js";
-import callUpRoutes from "./src/routes/callUp.routes.js";
-import getUserRoutes from "./src/routes/getUser.routes.js";
-import profileRoutes from "./src/routes/profile.routes.js";
+import { PORT } from "./src/utils/env";
+import appRoutes from "./src/routes/appRoutes.routes";
+import DB from "./src/config/db";
 
 const server = express();
 
@@ -15,21 +10,9 @@ server.use(cors({ origin: ["http://localhost:5173", "deployed-url"] }));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use("/api/create-stream", roomRoutes);
-// server.use("/api", roomRoutes);
+server.use("/api", appRoutes);
 
-server.use("/api/access-stream", accessTokenRoutes);
-
-server.use("/api/auth-user", authUserRoutes);
-
-server.use("/api/sign-up", signUpRoutes);
-
-server.use("/api/call-up", callUpRoutes);
-
-server.use("/api/getUser", getUserRoutes);
-
-server.use("/api/get-profile", profileRoutes);
-
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
   console.log(`✅ Server is running on port ${PORT}`);
+  await DB();
 });
