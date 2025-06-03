b0a51e3b034588c55014864411806eef2ae371db
cakezero <sareahj.com@gmail.com>
2025-06-03 17:28:18 +0100

    deleted unnecessary routes

--- server/src/routes/accessToken.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/accessToken.routes.ts
-import { Router } from "express";
-import { getAccessToken } from "../controllers/accessToken.controller.js";
-
-const router = Router();
-
-router.post("/", getAccessToken);
-
-export default router;
