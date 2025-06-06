0a4d1eadba8ad83efc76be8855084020bb03566d
cakezero <sareahj.com@gmail.com>
2025-06-06 20:01:28 +0100

    env update

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -5,6 +5,7 @@ export const ENV_SCHEMA = {
     ENVIRONMENT: import.meta.env.VITE_ENVIRONMENT || "development",
     PINATA_JWT: import.meta.env.VITE_PINATA_JWT as string,
     PAYMASTER_URL: `https://paymaster.biconomy.io/api/v2/84532/${import.meta.env.VITE_PAYMASTER_API_KEY!}`,
+    HUDDLE_PROJECT_ID: import.meta.env.VITE_HUDDLE_PROJECT_ID as string,
 };
 
 export const PUBLIC_ROUTES = ["/auth"];
@@ -12,3 +13,5 @@ export const PUBLIC_ROUTES = ["/auth"];
 export const network = ENV_SCHEMA.ENVIRONMENT === "development" ? baseSepolia : base;
 
 export const REVENUE_MANAGER_ADDRESS = `0x${""}`;
+
+export const BACKEND_URL = "http://localhost:4500";
