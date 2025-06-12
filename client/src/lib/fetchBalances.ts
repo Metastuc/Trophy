e9540c92f01062f3dcccbff65cde51e2246c1291
cakezero <sareahj.com@gmail.com>
2025-06-12 17:04:36 +0100

    balance update (portfolio PnL arrow and total money in wallet)

--- client/src/lib/fetchBalances.ts
+++ client/src/lib/fetchBalances.ts
@@ -14,16 +14,18 @@ type balanceType = {
     }
   },
   portfolio_percent: string;
+  portfolioArrow: string;
+  totalMoney: string;
 };
 
-/**
- * Fetches the token balances for a given wallet address.
- * @param {string} address The wallet address.
- * @returns {balanceType} A promise that resolves
- * to an object where the keys are the token symbols(ETH, ZORA, DEGEN, USDC) and the values are objects with the properties
- * "balance", "H24_change", "balance_price_usd" and "token_price_usd". The "balance" property is the balance of the token in the wallet,
- * the "balance_price_usd" property is the current value of 1 unit of the token in USD, the "token_price_usd" property is the current value of the token in USD and the "H24_change property is the change in the last 24 hours".
- */
+const toFixed = (value: string | number) => {
+  if (typeof value === "string") {
+    return parseInt(value).toFixed(2);
+  }
+
+  return value.toFixed(2);
+}
+
 export default async function GetTokenBalances(address: string): Promise<balanceType> {
   try {
     if (!moralisStarted) {
@@ -40,23 +42,32 @@ export default async function GetTokenBalances(address: string): Promise<balance
       tokenAddresses,
     });
 
-    const balances: balanceType = { tokens: {}, portfolio_percent: "" };
+    const balances: balanceType = { tokens: {}, portfolio_percent: "", portfolioArrow: "", totalMoney: "" };
     let arrow = "down";
+    let portfolioArrow = "down";
     let portfolioPercent = 0;
+    let totalMoney = 0;
 
     for (let i = 0; i < response.result.length; i++) {
+
       const H24_change = parseInt(response.result[i].usdValue24hrUsdChange ?? "0");
       if (H24_change > 0) arrow = "up";
       balances.tokens[response.result[i].symbol] = {
-        balance: response.result[i].balanceFormatted,
-        balance_price_usd: response.result[i].usdValue.toString(),
-        token_price_usd: response.result[i].usdPrice,
+        balance: toFixed(response.result[i].balanceFormatted),
+        balance_price_usd: toFixed(response.result[i].usdValue),
+        token_price_usd: toFixed(response.result[i].usdPrice),
         arrow,
         H24_change: H24_change.toFixed(2),
       };
+      totalMoney += response.result[i].usdValue;
       portfolioPercent += response.result[i].portfolioPercentage;
     }
-    balances.portfolio_percent = portfolioPercent.toFixed(2).toString();
+
+    if (portfolioPercent > 0) portfolioArrow = "up"
+
+    balances.portfolio_percent = toFixed(portfolioPercent);
+    balances.portfolioArrow = portfolioArrow;
+    balances.totalMoney = toFixed(totalMoney);
 
     return balances;
   } catch (error) {
