ec8faeb84fbf519f070286ca6ae1abcd56d07de0
cakezero <sareahj.com@gmail.com>
2025-06-12 17:06:16 +0100

    remane from updateProfile to updateUser (added fees logic to update total fees gotten by user)

--- server/src/controllers/updateUser.controller.ts
+++ server/src/controllers/updateUser.controller.ts
@@ -37,3 +37,23 @@ export const updateProfile = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+
+export const feesUpdate = async (req: Request, res: Response) => {
+	try {
+		const { username, fees } = req.body;
+		const user = await User.findOne({ username });
+		if (!user) {
+			res.status(404).json({ message: "user not found :(" });
+			return;
+		}
+
+		user.totalFees += fees;
+		await user.save();
+
+		res.status(200).json({ message: "fees updated :)" });
+	} catch (error: any) {
+		console.error(error);
+		res.status(500).json({ error: error.message })
+	}
+}
