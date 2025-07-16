5edd2aae22856723494ee471852cdaedf5333393
cakezero <sareahj.com@gmail.com>
2025-07-16 22:22:55 +0100

    chore: notis done (tip, follow, buy)

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import appRoutes from "./src/routes/appRoutes.routes";
 import DB from "./src/config/db";
 import { PORT } from "./src/utils/env";
 import { Server } from "socket.io";
-import { User } from "./src/models/userSchema";
+import { User, Notification } from "./src/models/userSchema";
 
 const server = express();
 
@@ -32,43 +32,114 @@ io.on("connection", (socket) => {
     userSocketIds.set(data.username, socket.id);
   });
 
-  socket.on("followed", async (data) => {
-    console.log(`🔔 User followed: ${data}`);
-    const { email, username } = data; // username is the reciever's username
+  const toLocaleString = (number: string) => {
+    return Number(number).toFixed(1).toLocaleString();
+  };
 
+  function formatNumber(num: string) {
+    const amount = Number(num);
+    if (amount >= 1000000000000) return (amount / 1000000000000).toFixed(1).replace(/\.0$/, '') + 'T';
+    if (amount >= 1000000000) return (amount / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+    if (amount >= 1000000) return (amount / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    if (amount >= 1000) return (amount / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    return amount.toString();
+  }
+
+  socket.on("followed", async (data) => {
+    const { email, username } = data;
     const follower = await User.findOne({ email });
     const reciever = await User.findOne({ username });
 
-    if (follower && reciever) {
-      follower.following.push(username);
-      reciever.followers.push(follower.username);
-      reciever.notifications.length === 6 ? reciever.notifications.pop() : "";
-      reciever.notifications.push(`${follower.username} followed you!`);
-      await follower.save();
-      await reciever.save();
+    if (!follower || !reciever) return;
 
-      const recieverSocketId = userSocketIds.get(username);
-      io.to(recieverSocketId).emit("followed", data);
+    follower.following.push(username);
+    reciever.followers.push(username);
+
+    const followMessage = `${follower.username} followed you`;
+
+    const now = new Date();
+    const notification = await Notification.findOne({ username });
+
+    if (!notification) {
+      await Notification.create({ username, follow: { followNots: [followMessage], follwedAt: now } });
+      const recieverSocketId = userSocketIds.get(reciever.username);
+
+      io.to(recieverSocketId).emit("followed");
+      return;
     }
+
+    const recentFollow = (now.getTime() - new Date(notification.follow!.followedAt).getTime()) < 60 * 60 * 1000;
+    let content = notification.follow!.content;
+
+    if (recentFollow) {
+      content = `${username} and ${notification.follow!.recentFollows} others followed you`;
+      notification.follow!.recentFollows += 1;
+      notification.follow!.followNots[0] = content;
+    } else {
+      notification.follow!.followNots.push(followMessage);
+      notification.follow!.content = followMessage;
+      notification.follow!.recentFollows = 1;
+    }
+
+    await follower.save();
+    await reciever.save();
+    await notification.save();
+
+    const recieverSocketId = userSocketIds.get(reciever.username);
+    io.to(recieverSocketId).emit("followed");
   });
 
   socket.on("send-tip-notis", async (tipData) => {
-    console.log(`💰 Tip sent: ${tipData}`);
-
     const { email, username, token, amount } = tipData;
 
-    const follower = await User.findOne({ email });
+    const sender = await User.findOne({ email });
     const reciever = await User.findOne({ username });
 
-    if (follower && reciever) {
-      reciever.notifications.length === 6 ? reciever.notifications.pop() : "";
-      reciever.notifications.push(`${follower.username} tipped you ${amount} ${token}!`);
-      await reciever.save();
+    if (!sender || !reciever) return;
+
+    const notification = await Notification.findOne({ username });
+
+    const localeAmount = toLocaleString(amount as string)
+
+    const tipMessage = { tipper: sender.username, amount: localeAmount, token }
+
+    if (!notification) {
+      await Notification.create({
+        username,
+        tip: [tipMessage],
+      });
+      const recieverSocketId = userSocketIds.get(username);
+
+      io.to(recieverSocketId).emit("tipped");
+      return;
     }
 
-    const recieverSocketId = userSocketIds.get(tipData.username);
-    io.to(recieverSocketId).emit("tipped", tipData);
+    notification.tip.push(tipMessage);
+
+    await reciever.save();
+    await notification.save();
+
+    const recieverSocketId = userSocketIds.get(reciever.username);
+    io.to(recieverSocketId).emit("tipped");
   });
+
+  socket.on("buy", async (data) => {
+    const { buyer, streamer, amount } = data
+    const notification = await Notification.findOne({ username: streamer });
+    
+    const buyMessage = `${buyer} bought ${formatNumber(amount)} of your troph`
+
+    if (!notification) {
+      await Notification.create({ buy: [buyMessage] })
+      return
+    }
+
+    notification.buy.push(buyMessage)
+    await notification.save()
+
+    const recieverSocketId = userSocketIds.get(streamer);
+    io.to(recieverSocketId).emit("buy");
+  })
 
   socket.on("join-chat", (data) => {
     socket.join(data.roomId);
