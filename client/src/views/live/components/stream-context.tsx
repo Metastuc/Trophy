b19ba6cbd45e02fac04fd9713d669e419e7b9a50
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-01 00:32:29 +0100

    update stream-context.tsx

--- client/src/views/live/components/stream-context.tsx
+++ client/src/views/live/components/stream-context.tsx
@@ -9,18 +9,21 @@ export function StreamContext() {
 
     return (
         <section className="mt-2 mb-8.5">
-            <aside className="flex items-center justify-start gap-1 my-2">
+            <aside className="my-2 flex items-center justify-start gap-1">
                 <div className="size-8.5">
-                    <StreamerLivePFP imageSrc="https://placehold.co/400x400/pink/blue" imageAlt="" />
+                    <StreamerLivePFP
+                        imageSrc="https://placehold.co/400x400/pink/blue"
+                        imageAlt={`${streamCreator.user.username}-pfp`}
+                    />
                 </div>
                 <span className="text-sm">@{streamCreator.user.username}</span>
 
-                <button className="bg-blue100 rounded px-3 py-0.5 ml-2">
+                <button className="bg-blue100 ml-2 rounded px-3 py-0.5">
                     <span className="text-xs text-white">Follow</span>
                 </button>
             </aside>
 
-            <aside className="">
+            <aside>
                 <span>{streamResponse.title}</span>
             </aside>
         </section>
