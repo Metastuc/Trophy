67e986e21fa714f123c528acd41f8776724c28d4
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-01 06:20:05 +0100

    update bun.lock, package.json and profile.tsx

--- client/src/routes/_app/profile.tsx
+++ client/src/routes/_app/profile.tsx
@@ -1,46 +1,60 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect, useLoaderData } from "@tanstack/react-router";
 
+import { getUser } from "@/api/get-user";
 import { PageContentLayout } from "@/components/layouts/main-content";
+import { toast } from "sonner";
 
 export const Route = createFileRoute("/_app/profile")({
+    async beforeLoad({ context }) {
+        if (!context.authenticationStore?.isAuthenticated) {
+            toast.error("You must be logged in to view your profile");
+            throw redirect({ to: "/" });
+        }
+    },
+
+    async loader({ context }) {
+        const response = await context.queryClient.ensureQueryData(
+            getUser({ username: context.authenticationStore?.user?.backendUserData.user.username as string }),
+        );
+
+        if (!response) {
+            throw new Error("Unable to get user profile");
+        }
+
+        return { user: response.user, streams: response.stream };
+    },
+
     component() {
+        const { user } = useLoaderData({ from: "/_app/profile" });
+
         return (
             <PageContentLayout>
                 <section className="flex gap-1">
-                    {/* <aside className="flex w-15 items-center justify-center">
-                        {status === "pending" ? (
-                            <Skeleton className="size-14 rounded-full" />
-                        ) : (
-                            <img
-                                alt="user-pfp"
-                                className="size-14 rounded-full"
-                                src={userData?.uploadedPfp}
-                            />
-                        )}
+                    <aside className="flex w-15 items-center justify-center">
+                        {/* <img alt="user-pfp" className="size-14 rounded-full" src={user.userPfp} /> */}
+                        <img
+                            alt="user-pfp"
+                            className="size-14 rounded-full object-cover"
+                            src="https://www.dummyimage.com/200x200/000/fff"
+                        />
                     </aside>
 
                     <aside className="flex-1 space-y-1">
-                        <h3>
-                            {status !== "pending" ? (
-                                <Skeleton className="h-4 w-1/4" />
-                            ) : (
-                                <span className="font-medium">@{userData?.username}</span>
-                            )}
-                        </h3>
+                        <h3 className="font-medium">@{user.username}</h3>
 
-                        {status === "pending" ? (
-                            <Skeleton className="h-8 w-1/2" />
-                        ) : (
-                            <p className="max-h-16 min-h-8 w-48 overflow-hidden text-sm">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-                                modi vel, asperiores commodi blanditiis suscipit fugiat saepe error.
-                                Non, natus! Cupiditate, temporibus quaerat! Ullam ea perferendis
-                                velit, blanditiis nam possimus tempora, neque ipsam sint nesciunt
-                                error molestiae? Soluta aperiam tenetur nulla eos dolorem deserunt
-                                nesciunt maxime, architecto unde perspiciatis maiores!
-                            </p>
-                        )}
-                    </aside> */}
+                        {user.bio ? <p className="max-h-16 min-h-8 w-48 overflow-hidden text-sm">{user.bio}</p> : null}
+
+                        <div className="flex gap-2">
+                            <span className="">
+                                <b className="text-blue100 font-normal">{user.following.length}</b> Following
+                            </span>
+                            <span className="">
+                                <b className="text-blue100 font-normal">{user.followers.length}</b> Followers
+                            </span>
+                        </div>
+                    </aside>
+
+                    <aside></aside>
                 </section>
             </PageContentLayout>
         );
