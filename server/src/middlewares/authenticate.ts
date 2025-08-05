7c8331e3878a576453305b2ab98a7e95e59f601e
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-05 21:39:22 +0100

    update authenticate.ts

--- server/src/middlewares/authenticate.ts
+++ server/src/middlewares/authenticate.ts
@@ -13,8 +13,6 @@ export async function authenticate(request: Request, response: Response, next: N
     return;
   }
 
-  console.log(authorizationHeader);
-
   try {
     request.privyUser = await privy.verifyAuthToken(authorizationHeader.split(" ")[1], PRIVY_KEY);
 
