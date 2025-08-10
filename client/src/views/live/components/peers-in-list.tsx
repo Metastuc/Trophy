e40928d186d66d7ca5c77d751c6f9a1ccfb77d5c
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-10 17:29:53 +0100

    update peers-in-list.tsx

--- client/src/views/live/components/peers-in-list.tsx
+++ client/src/views/live/components/peers-in-list.tsx
@@ -3,13 +3,11 @@ import { UserCheck } from "lucide-react";
 import { useMemo } from "react";
 
 export function AuthenticatedPeersList({ onToggle, peerId, search, selected }: iAuthenticatedPeersList) {
-    const { metadata } = useRemotePeer({ peerId }) as { metadata: tStreamUIMetadata };
-    const isAuthenticated = Boolean(metadata?.username && metadata.username !== "anon");
+    const { metadata } = useRemotePeer<tStreamUIMetadata>({ peerId });
+    const isAuthenticated = Boolean(metadata?.username !== "anon");
 
     const queryMatches = useMemo(
-        function () {
-            return isAuthenticated && metadata.username.toLowerCase().includes(search.toLowerCase());
-        },
+        () => isAuthenticated && metadata?.username.toLowerCase().includes(search.toLowerCase()),
         [isAuthenticated, metadata, search],
     );
 
@@ -20,13 +18,13 @@ export function AuthenticatedPeersList({ onToggle, peerId, search, selected }: i
             <span className="flex size-8 items-center justify-center overflow-hidden rounded-full bg-gradient-to-b from-[#6055FF] to-[#3A3399FC]">
                 <i className="flex size-7 items-center justify-center">
                     <img
-                        src={metadata.userPFP}
-                        alt={`${metadata.username}-pfp`}
+                        src={metadata?.userPFP}
+                        alt={`${metadata?.username}-pfp`}
                         className="rounded-full object-cover"
                     />
                 </i>
             </span>
-            <span>@{metadata.username}</span>
+            <span>@{metadata?.username}</span>
 
             {selected ? (
                 <i className="text-blue100 ml-auto size-3.5">
@@ -38,20 +36,20 @@ export function AuthenticatedPeersList({ onToggle, peerId, search, selected }: i
 }
 
 export function SelectedPeersList({ peerId }: { peerId: string }) {
-    const { metadata } = useRemotePeer({ peerId }) as { metadata: tStreamUIMetadata };
+    const { metadata } = useRemotePeer<tStreamUIMetadata>({ peerId });
 
     return (
         <li className="flex w-max items-center gap-1 rounded-3xl bg-gray-200 p-1 pr-2">
             <span className="flex size-6 items-center justify-center overflow-hidden rounded-full bg-gradient-to-b from-[#6055FF] to-[#3A3399FC]">
                 <i className="flex size-5 items-center justify-center">
                     <img
-                        src={metadata.userPFP}
-                        alt={`${metadata.username}-pfp`}
+                        src={metadata?.userPFP}
+                        alt={`${metadata?.username}-pfp`}
                         className="rounded-full object-cover"
                     />
                 </i>
             </span>
-            <span className="text-sm">@{metadata.username}</span>
+            <span className="text-sm">@{metadata?.username}</span>
         </li>
     );
 }
