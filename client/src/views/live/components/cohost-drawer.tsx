515f5c06dc23d721b82fe05eeb64cee6ea2f7084
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-10 06:29:52 +0100

    update cohost-drawer.tsx

--- client/src/views/live/components/cohost-drawer.tsx
+++ client/src/views/live/components/cohost-drawer.tsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react";
+import { Projector, Search, X } from "lucide-react";
 import { ChangeEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -26,35 +26,40 @@ export function CoHostDrawer() {
             repositionInputs={false}
         >
             <DrawerContent>
-                <DrawerHeader className="flex flex-row items-center justify-start">
-                    <DrawerTitle className="flex">
-                        <Button variant="ghost" onClick={() => setIsCoHostDrawerOpen(false)}>
-                            <X />
+                <DrawerHeader className="relative flex flex-row items-center justify-center">
+                    <DrawerTitle className="absolute left-4 flex p-0">
+                        <Button variant="ghost" onClick={() => setIsCoHostDrawerOpen(false)} className="size-5 !p-0">
+                            <X className="size-5" />
                         </Button>
                     </DrawerTitle>
-                    <DrawerDescription className="text-blue100 text-base font-medium">
+                    <DrawerDescription className="text-blue100 pt-0.5 text-lg font-medium">
                         Add co-streamers
                     </DrawerDescription>
                 </DrawerHeader>
 
                 <DrawerFooter>
-                    <header>
+                    <header className="border-blue100 space-y-2 rounded-xs border px-3 py-2">
                         <div>display</div>
-                        <div>
-                            <i></i>
+                        <div className="flex items-center justify-start gap-1 text-[#060606B2]">
+                            <i className="size-4">
+                                <Search />
+                            </i>
                             <input
                                 type="text"
                                 placeholder="Search by username"
                                 value={searchQuery.trim()}
                                 onChange={(event: ChangeEvent<HTMLInputElement>) => setSearchQuery(event.target.value)}
+                                className="outline-none"
                             />
                         </div>
                     </header>
 
                     <footer>
-                        <div>
-                            <span>People on your livestream</span>
-                            <i>icon</i>
+                        <div className="mt-6 mb-3 flex items-center justify-start gap-1">
+                            <span className="text-blue100 font-medium">People on your livestream</span>
+                            <i className="size-4 text-[#E120E1]">
+                                <Projector />
+                            </i>
                         </div>
 
                         <ul className="flex max-h-90 flex-col gap-2 overflow-auto">
