97e2fa41d15b0262d33669c3bb7b7cb1a4d2ddd0
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-27 16:07:43 +0100

    update constants.ts and smart-account.ts

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -2,13 +2,13 @@ import Moralis from "moralis";
 import type { Address } from "viem";
 import { base, baseSepolia } from "viem/chains";
 
-import { BANKR,DEGEN, USDC, ZORA } from "./contracts";
+import { BANKR, DEGEN, USDC, ZORA } from "./contracts";
 
 export const ENV_SCHEMA = {
     ENVIRONMENT: import.meta.env.VITE_ENVIRONMENT as "development" | "production",
     HUDDLE_PROJECT_ID: import.meta.env.VITE_HUDDLE_PROJECT_ID as string,
-    MORALIS_API_KEY: import.meta.env.VITE_MORALIS_API_KEY!,
-    PAYMASTER: import.meta.env.VITE_PAYMASTER_URL!,
+    MORALIS_API_KEY: import.meta.env.VITE_MORALIS_API_KEY as string,
+    PAYMASTER: import.meta.env.VITE_PAYMASTER_URL as string,
     PINATA_JWT: import.meta.env.VITE_PINATA_JWT as string,
     PRIVY_APP_ID: import.meta.env.VITE_PRIVY_APP_ID as string,
     PRIVY_CLIENT_ID: import.meta.env.VITE_PRIVY_CLIENT_ID as string,
