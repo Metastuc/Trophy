9c6cb89ab04d1bbac468c6affb5767e50e34fde7
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-08-29 17:34:38 +0100

    update 6 files

--- client/src/views/tip-drawer/index.tsx
+++ client/src/views/tip-drawer/index.tsx
@@ -3,26 +3,31 @@ import { CircleDollarSign } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerTrigger } from "@/components/ui/drawer";
 
-import { TradeDrawerContextProvider } from "../trade-modal/context";
+import { logger } from "@/utils/logger";
+import { TipDrawerContextProvider } from "./context";
 import { useTipDrawerContext } from "./hooks";
 
 export function TipDrawer(props: TipDrawer) {
+    logger({ props });
+
     return (
-        <TradeDrawerContextProvider {...props}>
+        <TipDrawerContextProvider {...props}>
             <TipDrawerInner />
-        </TradeDrawerContextProvider>
+        </TipDrawerContextProvider>
     );
 }
 
 function TipDrawerInner() {
-    const { closeDrawer, isDrawerOpen, openDrawer } = useTipDrawerContext();
+    const { closeDrawer, isDrawerOpen, openDrawer, streamerWalletAddress } = useTipDrawerContext();
+
+    logger({ streamerWalletAddress });
 
     return (
         <Drawer open={isDrawerOpen} onOpenChange={(isOpen) => (isOpen ? openDrawer() : closeDrawer())}>
             <DrawerTrigger asChild>
                 <Button
                     variant="default"
-                    className="flex h-6 w-20 items-center justify-center gap-1 rounded bg-gradient-to-b from-[#2D57FF] to-[#1B3499] text-white"
+                    className="flex h-6 w-20 items-center justify-center gap-1 rounded bg-gradient-to-b from-[#2D57FF] to-[#1B3499] p-0 text-white"
                 >
                     <i className="size-4">
                         <CircleDollarSign />
