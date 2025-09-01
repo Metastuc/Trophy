3813f21fa4e31549c82b7dd7c2cb17bccaca9fb0
cakezero <sareahj.com@gmail.com>
2025-09-02 00:42:09 +0100

    push

--- client/src/lib/flaunch.ts
+++ client/src/lib/flaunch.ts
@@ -78,9 +78,9 @@ export const createCreatorToken = async (
             functionName: "flaunch",
             args: [
                 flaunchParams._flaunchParams,
-                // flaunchParams._whitelistParams,
-                // flaunchParams._airdropParams,
-                // flaunchParams._treasuryManagerParams,
+                flaunchParams._whitelistParams,
+                flaunchParams._airdropParams,
+                flaunchParams._treasuryManagerParams,
             ],
             account: smartWalletClient.account,
         });
