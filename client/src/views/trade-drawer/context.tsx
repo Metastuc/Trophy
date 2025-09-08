c52cfa9e96bc7c31e0fcc8662d4c676e999530e2
cakezero <sareahj.com@gmail.com>
2025-09-08 11:26:08 +0100

    test

--- client/src/views/trade-drawer/context.tsx
+++ client/src/views/trade-drawer/context.tsx
@@ -32,6 +32,7 @@ export function TradeDrawerContextProvider({ children, streamer }: TradeDrawerCo
                 await wallets[0].switchChain(network.id);
                 const provider: EIP1193Provider = await wallets[0].getEthereumProvider();
 
+                console.log(drawerData);
                 await buyCreatorToken(
                     streamer?.tokenAddress as Address,
                     drawerData.buyAmount,
@@ -43,7 +44,7 @@ export function TradeDrawerContextProvider({ children, streamer }: TradeDrawerCo
                 // optionally show a toast or error UI here
             }
         },
-        [drawerData.buyAmount, streamer?.tokenAddress, wallets],
+        [drawerData, streamer?.tokenAddress, wallets],
     );
 
     const value = useMemo(
