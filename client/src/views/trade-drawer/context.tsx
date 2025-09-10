58da679900e4617c22aca883a4d8178dd6c15d3b
cakezero <sareahj.com@gmail.com>
2025-09-10 08:09:08 +0100

    sell logic

--- client/src/views/trade-drawer/context.tsx
+++ client/src/views/trade-drawer/context.tsx
@@ -1,9 +1,10 @@
-import { EIP1193Provider, useWallets } from "@privy-io/react-auth";
+import { EIP1193Provider, useSignTypedData,useWallets } from "@privy-io/react-auth";
 import { PropsWithChildren, useCallback, useMemo, useState } from "react";
 import type { Address } from "viem";
 
 import { network } from "@/lib/constants";
-import { buyCreatorToken } from "@/lib/flaunch";
+// import { buyCreatorToken } from "@/lib/flaunch";
+import { sellCreatorToken } from "@/lib/flaunch";
 
 import { TradeDrawerContext } from "./hooks";
 
@@ -11,6 +12,7 @@ type TradeDrawerContextProvider = PropsWithChildren<TradeDrawer>;
 
 export function TradeDrawerContextProvider({ children, streamer }: TradeDrawerContextProvider) {
     const { wallets } = useWallets();
+    const { signTypedData } = useSignTypedData();
 
     const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
     const [drawerData, setDrawerData] = useState<TradeDrawerDataState>(() => ({
@@ -32,19 +34,28 @@ export function TradeDrawerContextProvider({ children, streamer }: TradeDrawerCo
                 await wallets[0].switchChain(network.id);
                 const provider: EIP1193Provider = await wallets[0].getEthereumProvider();
 
+
                 console.log(drawerData);
-                await buyCreatorToken(
+                // await buyCreatorToken(
+                //     streamer?.tokenAddress as Address,
+                //     drawerData.sellAmount,
+                //     provider,
+                //     wallets[0].address as Address,
+                // );
+
+                await sellCreatorToken(
                     streamer?.tokenAddress as Address,
                     drawerData.sellAmount,
                     provider,
+                    signTypedData,
                     wallets[0].address as Address,
-                );
+                )
             } catch (error) {
                 console.error("Swap failed:", error);
                 // optionally show a toast or error UI here
             }
         },
-        [drawerData, streamer?.tokenAddress, wallets],
+        [drawerData, signTypedData, streamer?.tokenAddress, wallets],
     );
 
     const value = useMemo(
