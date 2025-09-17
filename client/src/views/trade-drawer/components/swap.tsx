031188b03c0bde9ce71b0cd4fbd14337ce7970ac
cakezero <sareahj.com@gmail.com>
2025-09-17 08:23:47 +0100

    push

--- client/src/views/trade-drawer/components/swap.tsx
+++ client/src/views/trade-drawer/components/swap.tsx
@@ -139,11 +139,13 @@ export function Swap() {
     const { streamer, drawerData, setDrawerData } = useTradeDrawerContext();
     const { wallets } = useWallets();
     const { data } = useTokenPrice(TOKEN_ADDRESSES.ETH as Address);
+    console.log({data})
 
     useEffect(() => {
         (async () => {
             const { ethBal, tokenBal } = await getViewerBalance(streamer!.tokenAddress, wallets[0].address as Address);
             const tokenPrice = await getCreatorTokenPrice(streamer?.tokenAddress as Address);
+            console.log({tokenPrice})
             if (drawerData.from.type !== "native") {
                 setDrawerData((state) => ({
                     from: {
@@ -154,7 +156,7 @@ export function Swap() {
                     to: {
                         ...state.to,
                         balance: ethBal,
-                        usdPrice: data!.usdPrice.toString()
+                        usdPrice: data!.usdPrice.toString() || "0"
                     }
                 }));
             } else {
@@ -162,7 +164,7 @@ export function Swap() {
                     from: {
                         ...state.from,
                         balance: ethBal,
-                        usdPrice: data!.usdPrice.toString()
+                        usdPrice: data!.usdPrice.toString() || "0"
                     },
                     to: {
                         ...state.to,
