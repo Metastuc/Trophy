85d75d758ba2353e74a56027ed202763d6edf46b
Bright Senpai <sareahj.com@gmail.com>
2025-09-18 06:05:25 -0700

    Update get-token-prices.ts

--- client/src/api/get-token-prices.ts
+++ client/src/api/get-token-prices.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Address } from "viem";
 
-import { APPLICATION_CONSTANTS } from "@/lib/constants";
+import { APPLICATION_CONSTANTS, ENV_SCHEMA } from "@/lib/constants";
 // import MoralisClient from "@/lib/moralis";
 
 export function useTokenPrice(address: Address) {
@@ -14,6 +14,12 @@ export function useTokenPrice(address: Address) {
             return await axios
                 .get(
                     `https://deep-index.moralis.io/api/v2.2/erc20/${address}/price?chain=${APPLICATION_CONSTANTS.CURRENT_MORALIS_CHAIN}`,
+                    { 
+                        headers: {
+                            accept: 'application/json',
+                            'X-API-Key': ENV_SCHEMA.MORALIS_API_KEY
+                        }     
+                    }
                 )
                 .then((response) => response.data);
         },
