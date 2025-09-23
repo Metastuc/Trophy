75e12e688b85fed7e56f29c0eefec0cc630a17a0
cakezero <sareahj.com@gmail.com>
2025-09-23 13:53:35 +0100

    commented out the alchemy logic

--- client/src/lib/alchemy-sa.ts
+++ client/src/lib/alchemy-sa.ts
@@ -1,21 +1,21 @@
-import { createLightAccountAlchemyClient } from "@alchemy/aa-alchemy";
-import { chains, type SmartAccountSigner, WalletClientSigner } from "@alchemy/aa-core";
-import { EIP1193Provider } from "@privy-io/react-auth"
-import { Address } from "viem";
+// import { createLightAccountAlchemyClient } from "@alchemy/aa-alchemy";
+// import { chains, type SmartAccountSigner, WalletClientSigner } from "@alchemy/aa-core";
+// import { EIP1193Provider } from "@privy-io/react-auth"
+// import { Address, WalletClient } from "viem";
 
-import { ENV_SCHEMA, network } from "./constants";
-import { getWalletClient } from "./viem";
+// import { ENV_SCHEMA, network } from "./constants";
+// import { getWalletClient } from "./viem";
 
-export const alchemySmartAccount = async ({ provider, address }: { provider: EIP1193Provider, address: Address }) => {
-  const walletClient = getWalletClient(provider, address);
-  const signer: SmartAccountSigner = new WalletClientSigner(
-    walletClient,
-    "json-rpc"
-  );
+// export const alchemySmartAccount = async ({ provider, address }: { provider: EIP1193Provider, address: Address }) => {
+//   const walletClient = getWalletClient(provider, address) as unknown as WalletClient;
+//   const signer: SmartAccountSigner = new WalletClientSigner(
+//     walletClient!,
+//     "json-rpc"
+//   );
 
-  return await createLightAccountAlchemyClient({
-    signer,
-    chain: network as chains.Chain,
-    apiKey: ENV_SCHEMA.ALCHEMY_API_KEY
-  });
-}
+//   return await createLightAccountAlchemyClient({
+//     signer,
+//     chain: network as chains.Chain,
+//     apiKey: ENV_SCHEMA.ALCHEMY_API_KEY
+//   });
+// }
