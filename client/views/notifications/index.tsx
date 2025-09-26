3a189c902adf96eb091759404128a184d02f925e
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-09-26 17:04:19 +0100

    refactor(notifications): Simplify index component

--- client/views/notifications/index.tsx
+++ client/views/notifications/index.tsx
@@ -2,16 +2,16 @@ import { FollowNotification } from "./components/follow";
 import { PurchaseNotification } from "./components/purchase";
 import { TipNotification } from "./components/tip";
 
-export function RenderNotification(props: UserNotificationsData[number]["items"][number]) {
-    switch (props.type) {
+export function RenderNotification({ type, ...rest }: UserNotificationsData[number]["items"][number]) {
+    switch (type) {
         case "FOLLOW":
-            return <FollowNotification />;
+            return <FollowNotification {...rest} />;
 
         case "PURCHASE":
-            return <PurchaseNotification />;
+            return <PurchaseNotification {...rest} />;
 
         case "TIP":
-            return <TipNotification />;
+            return <TipNotification {...rest} />;
 
         default:
             return null;
