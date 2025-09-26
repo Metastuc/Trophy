06fecbada3868aaa4781b9189df82471281151cd
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-09-26 10:44:41 +0100

    refactor(routes): regenerate client route tree and update server routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 
-import { user } from "#app/authentication/user.controller.ts";
 import { getTokenPrice } from "#app/get-token-price.ts";
 
 import { authentication } from "./app/authentication";
 import { streams } from "./app/streams";
 import { follow } from "./app/subscription";
 import { tips } from "./app/tips";
+import { user } from "./app/user";
 
 export const routes = Router();
 
