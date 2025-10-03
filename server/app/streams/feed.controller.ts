4641ef2dad3bb7a12282b74ce68a633c9cd358a0
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-03 15:37:19 +0100

    feat(feed): Add pagination and filtering to feed controller to improve feed retrieval performance and provide clients with more flexible query options

--- server/app/streams/feed.controller.ts
+++ server/app/streams/feed.controller.ts
@@ -1,20 +1,40 @@
 import { NextFunction, Request, Response } from "express";
 
-import { PublicFeed, publicFeedSelectFields } from "#~/schema/feed/index.ts";
+import { FEED_RESPONSE_SCHEMA, publicFeedSelectFields } from "#~/schema/feed/index.ts";
 import { prisma } from "#config/prisma.ts";
 
-export async function publicFeedContent(_request: Request, response: Response, next: NextFunction) {
+export async function publicFeedContent(request: Request, response: Response, next: NextFunction) {
     try {
-        const liveStreams: Array<PublicFeed> = await prisma.stream.findMany({
-            where: { status: "LIVE" },
-            orderBy: { startedAt: "desc" },
-            select: publicFeedSelectFields,
-        });
+        const page = parseInt(request.query.page as string) || 1;
+        const limit = parseInt(request.query.limit as string) || 10;
+        const skip = (page - 1) * limit;
 
-        response.customResponse<Array<PublicFeed>>({
+        const [liveStreams, total] = await Promise.all([
+            prisma.stream.findMany({
+                where: { status: "LIVE" },
+                orderBy: { startedAt: "desc" },
+                skip,
+                take: limit,
+                select: publicFeedSelectFields,
+            }),
+
+            prisma.stream.count({
+                where: { status: "LIVE" },
+            }),
+        ]);
+
+        response.customResponse<PublicFeedData>({
             code: 200,
             message: "public feed content fetched successfully",
-            data: liveStreams,
+            data: FEED_RESPONSE_SCHEMA.parse({
+                items: liveStreams,
+                pagination: {
+                    hasNext: page * limit < total,
+                    limit,
+                    page,
+                    total,
+                },
+            }),
         });
     } catch (error) {
         next(error);
