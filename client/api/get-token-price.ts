92f7d890ce71b923849e78b47e59aadf3d9fa003
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-04 15:49:46 +0100

    refactor: Remove unused constants, simplify token‑price API, and tidy tsconfig to improve maintainability

--- client/api/get-token-price.ts
+++ client/api/get-token-price.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { Address } from "viem";
 
-import { API_ENDPOINTS, CLIENT_CONSTANTS } from "@/lib/constants";
+import { API_ENDPOINTS } from "@/lib/constants";
 import { makeRequest } from "#~/utils/axios.ts";
+import { toTime } from "#~/utils/time.ts";
 
 export function useTokenPrice(address: Address) {
     return useQuery({
-        queryKey: [CLIENT_CONSTANTS.QUERY_KEYS.PRICE.KEY, address],
+        queryKey: ["moralis-token-price", address],
         queryFn: async function () {
             return await makeRequest<TokenPriceResponse>({
                 method: "GET",
@@ -14,6 +15,6 @@ export function useTokenPrice(address: Address) {
             }).then((response) => response.data.data);
         },
         refetchInterval: false,
-        staleTime: CLIENT_CONSTANTS.QUERY_KEYS.PRICE.TTL,
+        staleTime: toTime({ unit: "hours", value: 1, output: "milliseconds" }),
     });
 }
