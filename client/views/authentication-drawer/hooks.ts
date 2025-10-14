f86d64d244b349d196fea0586bbf40832ad33613
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-14 10:06:40 +0100

    refactor(authentication): Remove deprecated onboarding and user controllers, update authentication hooks and index to simplify auth flow

--- client/views/authentication-drawer/hooks.ts
+++ client/views/authentication-drawer/hooks.ts
@@ -48,7 +48,7 @@ export function usePrivyLoginTrigger() {
                 closeDrawer();
                 login({ loginMethods: ["farcaster"] });
             }
-        },        
+        },
         [screen],
     );
 }
