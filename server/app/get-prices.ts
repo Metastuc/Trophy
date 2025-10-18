dae99e8a52ab7c61c2e85dbd0f8532ce0909a56f
cakezero <sareahj.com@gmail.com>
2025-10-18 12:20:11 +0100

    get eth price server logic

--- server/app/get-prices.ts
+++ server/app/get-prices.ts
@@ -1,9 +1,11 @@
+import axios from "axios";
 import { NextFunction, Request, Response } from "express";
 
-import { SERVER_CONSTANTS } from "#config/constants.ts";
+import { COINGECKO_URL, SERVER_CONSTANTS, SERVER_ENV } from "#config/constants.ts";
 import { MoralisClient } from "#config/moralis.ts";
 import { redis } from "#config/redis.ts";
 import { HttpError } from "#middleware/error.ts";
+
 
 export async function getTokenPrice(request: Request, response: Response, next: NextFunction) {
     const moralis = await MoralisClient();
@@ -38,6 +40,49 @@ export async function getTokenPrice(request: Request, response: Response, next: 
             code: 200,
             data: result,
             message: "token price fetched successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getEthPrice = async (request: Request, response: Response, next: NextFunction) => {
+    const { walletAddress } = request.query;
+
+    if (!walletAddress) {
+        throw new HttpError({ message: "wallet address is missing", code: 422 });
+    }
+
+    const cacheKey = SERVER_CONSTANTS.REDIS_KEYS.ETH_TOKEN_PRICE.KEY(walletAddress as string);
+
+    try {
+        const cached = await redis.get(cacheKey);
+        if (cached) {
+            response.customResponse<ETHPriceData>({
+                code: 200,
+                message: "eth price fetched from cache",
+                data: JSON.parse(cached),
+            });
+            return;
+        }
+
+        const req_options = {
+            url: COINGECKO_URL,
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "x-cg-demo-api-key": SERVER_ENV.COINGECKO_API_KEY
+            }
+        }
+
+        const result = await axios.request(req_options)
+            .then((response) => response.data);
+
+        await redis.set(cacheKey, JSON.stringify(result), "EX", SERVER_CONSTANTS.REDIS_KEYS.ETH_TOKEN_PRICE.TTL);
+        response.customResponse<ETHPriceData>({
+            code: 200,
+            data: result,
+            message: "eth price fetched successfully",
         });
     } catch (error) {
         next(error);
