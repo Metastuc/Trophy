c5b83068969b987a137c30d2fbd5f7964825ec32
cakezero <sareahj.com@gmail.com>
2025-10-18 12:20:36 +0100

    type property for eth data

--- shared/schema/token-price/types.d.ts
+++ shared/schema/token-price/types.d.ts
@@ -3,6 +3,13 @@ import type { GetTokenPriceResponseAdapter } from "@moralisweb3/common-evm-utils
 declare global {
     type TokenPriceData = GetTokenPriceResponseAdapter["raw"];
     type TokenPriceResponse = ApiResponse<TokenPriceData>;
+
+    type ETHPriceData = {
+        ethereum: {
+            usd: number;
+        };
+    };
+    type ETHPriceResponse = ApiResponse<ETHPriceData>;
 }
 
 export {};
