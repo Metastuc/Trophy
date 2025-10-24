07dfbca43c09b0ac49e26c92efba6bccc29b4315
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-24 23:05:03 +0100

    feat(schema): Add token schema and types, remove obsolete token-price types

--- shared/schema/token/index.ts
+++ shared/schema/token/index.ts
@@ -1,5 +1,10 @@
+import { isAddress } from "viem";
 import { z } from "zod";
 
 export const CREATE_TOKEN_URI_RESPONSE_SCHEMA = z.object({
     tokenUri: z.string(),
+});
+
+export const GET_TOKEN_PRICE_REQUEST_SCHEMA = z.object({
+    address: z.string().refine(isAddress, "Invalid token address"),
 });
