c60366068691c78782ade7abb6b4ae3ae7783608
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-24 15:23:35 +0100

    feat(farcaster): Add Farcaster context provider and hooks for client integration

--- client/contexts/farcaster.tsx
+++ client/contexts/farcaster.tsx
@@ -1,12 +1,28 @@
-import { sdk } from "@farcaster/miniapp-sdk";
+import sdk from "@farcaster/miniapp-sdk";
+import { usePrivy } from "@privy-io/react-auth";
+import { useLoginToMiniApp } from "@privy-io/react-auth/farcaster";
 import { ReactNode, useEffect } from "react";
 
 export function FarcasterMiniAppProvider({ children }: { children: ReactNode }) {
-    useEffect(function () {
-        (async function () {
-            await sdk.actions.ready();
-        })();
-    }, []);
+    const { ready, authenticated } = usePrivy();
+    const { initLoginToMiniApp, loginToMiniApp } = useLoginToMiniApp();
+
+    useEffect(
+        function () {
+            if (ready || !authenticated)
+                (async function () {
+                    const { nonce } = await initLoginToMiniApp();
+                    const result = await sdk.actions.signIn({ nonce });
+
+                    await loginToMiniApp({
+                        message: result.message,
+                        signature: result.signature,
+                    });
+                })();
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [ready, authenticated],
+    );
 
     return children;
 }
