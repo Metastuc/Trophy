278707e52105ead020ed10158ae7967c684acfdb
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-10-25 09:36:17 +0100

    refactor: Simplify media component by removing unused state and replacing it with useMediaQuery hook to improve readability and maintainability

--- client/views/live/components/media.tsx
+++ client/views/live/components/media.tsx
@@ -127,7 +127,7 @@ export const LiveStreamMedia = memo(function ({
                 )}
             >
                 {videoStream && videoStreamState === "playable" ? (
-                    <video autoPlay className="size-full object-cover" muted ref={videoRef} />
+                    <video autoPlay className="object-contain md:object-cover" muted ref={videoRef} />
                 ) : metadata ? (
                     <Fragment> {metadata} </Fragment>
                 ) : (
